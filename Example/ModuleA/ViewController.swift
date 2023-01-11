108362dc0704e10ffe4b5a2ad6d45974658aefe8
王欣 <wangxin102@58.com>
2023-01-11 14:54:43 +0800

    Initial commit

---
+++ Example/ModuleA/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ModuleA
+//
+//  Created by 王欣 on 01/11/2023.
+//  Copyright (c) 2023 王欣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
